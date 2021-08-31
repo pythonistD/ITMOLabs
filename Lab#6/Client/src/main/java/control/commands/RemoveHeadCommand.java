bc8524e439d2b3e31ff57bbba6fbc917f9c84b40
Cheboksarov Yaroslav <yacheboksarov768@gmail.com>
2021-09-01 00:03:19 +0300

    Добавил команду RemoveHeadCommand

--- Lab#6/Client/src/main/java/control/commands/RemoveHeadCommand.java
+++ Lab#6/Client/src/main/java/control/commands/RemoveHeadCommand.java
@@ -3,15 +3,12 @@ package control.commands;
 import model.Dragon;
 
 public class RemoveHeadCommand extends Command {
+    private static final long serialVersionUID = 21L;
     /**
      * Запуск комманды
      * @throws Exception
      */
     @Override
     public void execute() {
-        System.out.print(Dragon.getDragonsCollection().getFirst().toString());
-        Dragon.getDragonsCollection().removeFirst().toString();
-        System.out.println("Удаление прошло успешно");
-
     }
 }
--- Lab#6/Server/src/main/java/control/commands/RemoveHeadCommand.java
+++ Lab#6/Server/src/main/java/control/commands/RemoveHeadCommand.java
@@ -1,17 +1,24 @@
 package control.commands;
 
+import control.Response;
 import model.Dragon;
 
 public class RemoveHeadCommand extends Command {
+    private static final long serialVersionUID = 21L;
+    private Response response;
     /**
      * Запуск комманды
      * @throws Exception
      */
     @Override
     public void execute() {
-        System.out.print(Dragon.getDragonsCollection().getFirst().toString());
-        Dragon.getDragonsCollection().removeFirst().toString();
-        System.out.println("Удаление прошло успешно");
+        response = new Response("removeHead",Dragon.getDragonsCollection().getFirst().toString() + "Удаление прошло успешно");
+        Dragon.getDragonsCollection().removeFirst();
 
+    }
+
+    @Override
+    public Response getResponse() {
+        return response;
     }
 }
